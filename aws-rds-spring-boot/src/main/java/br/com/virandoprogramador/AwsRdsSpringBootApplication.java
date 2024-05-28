f57e26866059ef0982936c65a6b78e288d000d42
GuilhermeJWT <guiromanno@gmail.com>
2024-05-28 13:01:33 -0300

    ADD: projeto - rds spring boot

---
+++ aws-rds-spring-boot/src/main/java/br/com/virandoprogramador/AwsRdsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.virandoprogramador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsRdsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsRdsSpringBootApplication.class, args);
+	}
+
+}
---
+++ aws-rds-spring-boot/src/test/java/br/com/virandoprogramador/AwsRdsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.virandoprogramador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsRdsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
