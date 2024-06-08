28c5f0dbb5d2d0a41119e4092afae0fb08b47c81
GuilhermeJWT <guiromanno@gmail.com>
2024-06-08 16:50:56 -0300

    ADD: Projeto Redis com Spring Boot

---
+++ redis-spring-boot/src/main/java/br/com/virandoprogramador/redis_spring_boot/RedisSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.virandoprogramador.redis_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisSpringBootApplication.class, args);
+	}
+
+}
---
+++ redis-spring-boot/src/test/java/br/com/virandoprogramador/redis_spring_boot/RedisSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.virandoprogramador.redis_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
