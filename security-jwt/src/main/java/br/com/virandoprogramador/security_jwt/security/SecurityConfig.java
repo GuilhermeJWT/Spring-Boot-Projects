bc7a962f7a4e3e1a2290671e370f813421594b15
GuilhermeJWT <guiromanno@gmail.com>
2024-06-08 16:49:13 -0300

    UPDATE: config

--- security-jwt/src/main/java/br/com/virandoprogramador/security_jwt/security/SecurityConfig.java
+++ security-jwt/src/main/java/br/com/virandoprogramador/security_jwt/security/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig { 
 
     @Autowired
     private UserAuthenticationFilter userAuthenticationFilter;
