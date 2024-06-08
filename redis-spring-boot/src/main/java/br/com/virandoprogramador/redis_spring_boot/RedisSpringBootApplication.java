250bc262655657020251282074c01076ee561c71
GuilhermeJWT <guiromanno@gmail.com>
2024-06-08 17:25:35 -0300

    ADD: Config iniciais do Redis

--- redis-spring-boot/src/main/java/br/com/virandoprogramador/redis_spring_boot/RedisSpringBootApplication.java
+++ redis-spring-boot/src/main/java/br/com/virandoprogramador/redis_spring_boot/RedisSpringBootApplication.java
@@ -2,7 +2,9 @@ package br.com.virandoprogramador.redis_spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class RedisSpringBootApplication {
 
