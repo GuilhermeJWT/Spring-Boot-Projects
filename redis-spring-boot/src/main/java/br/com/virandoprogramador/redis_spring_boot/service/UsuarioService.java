d0d345cd5b3089d31960f9fd20810f996ab46248
GuilhermeJWT <guiromanno@gmail.com>
2024-06-08 21:28:35 -0300

    ADD: implementando Caches na Camada de Service

--- redis-spring-boot/src/main/java/br/com/virandoprogramador/redis_spring_boot/service/UsuarioService.java
+++ redis-spring-boot/src/main/java/br/com/virandoprogramador/redis_spring_boot/service/UsuarioService.java
@@ -4,6 +4,9 @@ import br.com.virandoprogramador.redis_spring_boot.exception.UsuarioNaoEncontrad
 import br.com.virandoprogramador.redis_spring_boot.model.ModelUsuario;
 import br.com.virandoprogramador.redis_spring_boot.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,10 +18,12 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Cacheable(value = "ModelUsuario")
     public List<ModelUsuario> listarUsuarios(){
         return usuarioRepository.findAll();
     }
 
+    @Cacheable(value = "ModelUsuario", key = "#id")
     public ModelUsuario pesquisaPorId(Long id){
         Optional<ModelUsuario>  modelUsuario = usuarioRepository.findById(id);
         return modelUsuario.orElseThrow(() -> new UsuarioNaoEncontradoException("Usuario não encontrado"));
@@ -28,6 +33,7 @@ public class UsuarioService {
         return usuarioRepository.save(modelUsuario);
     }
 
+    @CachePut(value = "ModelUsuario", key = "#id")
     public ModelUsuario alterarUsuario(ModelUsuario modelUsuario, Long id){
         ModelUsuario usuarioAlterado = usuarioRepository.findById(id)
                 .orElseThrow(() -> new UsuarioNaoEncontradoException("Usuario não encontrado"));
@@ -37,6 +43,7 @@ public class UsuarioService {
         return usuarioRepository.save(usuarioAlterado);
     }
 
+    @CacheEvict(value = "ModelUsuario", key = "#id")
     public void deletarUsuario(Long id){
         ModelUsuario modelUsuario = usuarioRepository.findById(id)
                 .orElseThrow(() -> new UsuarioNaoEncontradoException("Usuario não encontrado"));
