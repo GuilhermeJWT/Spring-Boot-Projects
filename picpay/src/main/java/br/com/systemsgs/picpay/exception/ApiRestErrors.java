7575c3fee49c5bfbc7d176807bd5740b35fb87e8
GuilhermeJWT <guiromanno@gmail.com>
2024-06-27 17:34:07 -0300

    UPDATE: Altera o corpo do retorno da Exception - BAD REQUEST

--- picpay/src/main/java/br/com/systemsgs/picpay/exception/ApiRestErrors.java
+++ picpay/src/main/java/br/com/systemsgs/picpay/exception/ApiRestErrors.java
@@ -1,22 +1,27 @@
 package br.com.systemsgs.picpay.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
 
+@Getter
 public class ApiRestErrors {
 
+    private Instant timestamp;
+    private HttpStatus httpStatus;
     private List<String> erros;
 
-    public ApiRestErrors(List<String> erros) {
+    public ApiRestErrors(Instant timestamp, HttpStatus httpStatus, List<String> erros) {
+        this.timestamp = timestamp;
+        this.httpStatus = httpStatus;
         this.erros = erros;
     }
 
     public ApiRestErrors(String mensagemErro){
         this.erros = Arrays.asList(mensagemErro);
-    }
-
-    public List<String> getErros() {
-        return erros;
     }
 
 }
--- picpay/src/main/java/br/com/systemsgs/picpay/exception/PicPayExceptionHandlerAdvice.java
+++ picpay/src/main/java/br/com/systemsgs/picpay/exception/PicPayExceptionHandlerAdvice.java
@@ -1,11 +1,13 @@
 package br.com.systemsgs.picpay.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,7 +21,7 @@ public class PicPayExceptionHandlerAdvice {
                 .stream().map(erro -> erro.getDefaultMessage())
                 .collect(Collectors.toList());
 
-        return new ApiRestErrors(errors);
+        return new ApiRestErrors(Instant.now(), HttpStatus.BAD_REQUEST, errors);
     }
 
     @ExceptionHandler(CamposCpfEmailDuplicadosException.class)
