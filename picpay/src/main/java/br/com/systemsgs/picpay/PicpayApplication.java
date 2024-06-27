caa35bf7a92ff10317f9fcdf94fe362d763b938b
GuilhermeJWT <guiromanno@gmail.com>
2024-06-27 13:27:16 -0300

    ADD: Projeto Picpay

---
+++ picpay/src/main/java/br/com/systemsgs/picpay/PicpayApplication.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs.picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayApplication.class, args);
+	}
+
+}
