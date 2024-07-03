9af70f35e8104d7c809f1c10aee8b1a5b761f4d4
GuilhermeJWT <guiromanno@gmail.com>
2024-07-03 17:01:45 -0300

    ADD: Dados Estáticos das Transferências para Test

--- picpay/src/test/java/br/com/systemsgs/picpay/DadosEstaticosEntidades.java
+++ picpay/src/test/java/br/com/systemsgs/picpay/DadosEstaticosEntidades.java
@@ -1,11 +1,15 @@
 package br.com.systemsgs.picpay;
 
 import br.com.systemsgs.picpay.dto.TransferenciaDTO;
+import br.com.systemsgs.picpay.dto.TransferenciaResponseDTO;
 import br.com.systemsgs.picpay.entity.Carteira;
 import br.com.systemsgs.picpay.entity.CarteiraTipo;
+import br.com.systemsgs.picpay.entity.Transferencia;
 import lombok.Getter;
 
 import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
 
 @Getter
 public class DadosEstaticosEntidades {
@@ -22,6 +26,40 @@ public class DadosEstaticosEntidades {
         carteiraResponse.setCarteiraTipo(new CarteiraTipo());
 
         return carteiraResponse;
+    }
+
+    public Transferencia dadosTransferencia(){
+        Transferencia transferenciaResponse = new Transferencia();
+
+        transferenciaResponse.setId(UUID.randomUUID());
+        transferenciaResponse.setValor(BigDecimal.valueOf(10.0));
+        transferenciaResponse.setDataTransacao(Instant.now());
+        transferenciaResponse.setPagador(new Carteira());
+        transferenciaResponse.setRecebedor(new Carteira());
+
+        return transferenciaResponse;
+    }
+
+    public TransferenciaDTO dadosCreateTransferenciaDTO(){
+        TransferenciaDTO transferenciaDTO = new TransferenciaDTO();
+
+        transferenciaDTO.setPagador(Long.valueOf(2));
+        transferenciaDTO.setRecebedor(Long.valueOf(1));
+        transferenciaDTO.setValor(BigDecimal.valueOf(50.0));
+    }
+
+    public TransferenciaResponseDTO dadosTransferenciaResponse(){
+        TransferenciaResponseDTO transferenciaResponseDTO = new TransferenciaResponseDTO();
+
+        transferenciaResponseDTO.setIdTransferencia(UUID.randomUUID());
+        transferenciaResponseDTO.setIdPagador(Long.valueOf(1));
+        transferenciaResponseDTO.setNomePagador("Guilherme - Pagador");
+        transferenciaResponseDTO.setIdBeneficiario(Long.valueOf(2));
+        transferenciaResponseDTO.setNomeBeneficiario("Guilherme - Beneficiario");
+        transferenciaResponseDTO.setDataTransacao(Instant.now());
+        transferenciaResponseDTO.setValorTransferencia(BigDecimal.valueOf(50.0));
+
+        return transferenciaResponseDTO;
     }
 
     public CarteiraTipo dadosCarteiraTipoUser(){
