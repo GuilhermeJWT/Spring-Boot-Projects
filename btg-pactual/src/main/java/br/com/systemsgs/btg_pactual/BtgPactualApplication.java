9d01fa57b3b4c9d94e3260f479009a5d908a17da
GuilhermeJWT <guiromanno@gmail.com>
2024-07-05 09:00:22 -0300

    ADD: Projeto BTG Pactual - RabbitMQ com MongoDB

---
+++ btg-pactual/src/main/java/br/com/systemsgs/btg_pactual/BtgPactualApplication.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs.btg_pactual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtgPactualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtgPactualApplication.class, args);
+	}
+
+}
---
+++ btg-pactual/src/test/java/br/com/systemsgs/btg_pactual/BtgPactualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs.btg_pactual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtgPactualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
