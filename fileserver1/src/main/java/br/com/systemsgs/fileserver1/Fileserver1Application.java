d62fb7369212050ce5705a194ec1eb8c4c30ee10
GuilhermeJWT <guiromanno@gmail.com>
2024-07-06 17:04:12 -0300

    ADD: Projeto File Upload

---
+++ fileserver1/src/main/java/br/com/systemsgs/fileserver1/Fileserver1Application.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs.fileserver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fileserver1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fileserver1Application.class, args);
+	}
+
+}
---
+++ fileserver1/src/test/java/br/com/systemsgs/fileserver1/Fileserver1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs.fileserver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fileserver1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
