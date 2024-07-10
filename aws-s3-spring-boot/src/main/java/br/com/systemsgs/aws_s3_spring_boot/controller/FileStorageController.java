2db841965361fbd0944e15b152a6fb4cc1c97c6e
GuilhermeJWT <guiromanno@gmail.com>
2024-07-10 11:23:21 -0300

    ADD: Service e Controller - Upload/Download/Delete dos arquivos - S3

--- aws-s3-spring-boot/src/main/java/br/com/systemsgs/aws_s3_spring_boot/controller/FileStorageController.java
+++ aws-s3-spring-boot/src/main/java/br/com/systemsgs/aws_s3_spring_boot/controller/FileStorageController.java
@@ -1,4 +1,44 @@
 package br.com.systemsgs.aws_s3_spring_boot.controller;
 
+import br.com.systemsgs.aws_s3_spring_boot.service.FileService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/files")
 public class FileStorageController {
+
+    private final FileService fileService;
+
+    public FileStorageController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+        return new ResponseEntity<>(fileService.uploadFileS3(file), HttpStatus.OK);
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+        byte[] data = fileService.downloadFileS3(fileName);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity
+                .ok()
+                .contentLength(data.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
+    }
+
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+        return new ResponseEntity<>(fileService.deleteFileS3(fileName), HttpStatus.OK);
+    }
 }
--- aws-s3-spring-boot/src/main/java/br/com/systemsgs/aws_s3_spring_boot/service/FileService.java
+++ aws-s3-spring-boot/src/main/java/br/com/systemsgs/aws_s3_spring_boot/service/FileService.java
@@ -1,4 +1,66 @@
 package br.com.systemsgs.aws_s3_spring_boot.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.util.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@Slf4j
+@Service
 public class FileService {
+
+    @Value("${application.bucket.name}")
+    private String bucketName;
+
+    @Autowired
+    private AmazonS3 s3Client;
+
+    public String uploadFileS3(MultipartFile file) {
+        File fileObj = convertMultiPartFileToFile(file);
+        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+        s3Client.putObject(new PutObjectRequest(bucketName, fileName, fileObj));
+        fileObj.delete();
+        return "Arquivo salvo : " + fileName;
+    }
+
+
+    public byte[] downloadFileS3(String fileName) {
+        S3Object s3Object = s3Client.getObject(bucketName, fileName);
+        S3ObjectInputStream inputStream = s3Object.getObjectContent();
+        try {
+            byte[] content = IOUtils.toByteArray(inputStream);
+            return content;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public String deleteFileS3(String fileName) {
+        s3Client.deleteObject(bucketName, fileName);
+        return fileName + " removido ...";
+    }
+
+
+    private File convertMultiPartFileToFile(MultipartFile file) {
+        File convertedFile = new File(file.getOriginalFilename());
+        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
+            fos.write(file.getBytes());
+        } catch (IOException e) {
+            log.error("Erro ao converter multipartFile em arquivo", e);
+        }
+        return convertedFile;
+    }
 }
