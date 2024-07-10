d08ae4e67083de48fd6fcad9d50587a3b4d4c6f5
GuilhermeJWT <guiromanno@gmail.com>
2024-07-09 22:15:10 -0300

    ADD: Projeto Aws S3 - Download e Upload de Arquivos

---
+++ aws-s3-spring-boot/src/main/java/br/com/systemsgs/aws_s3_spring_boot/AwsS3SpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs.aws_s3_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsS3SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsS3SpringBootApplication.class, args);
+	}
+
+}
---
+++ aws-s3-spring-boot/src/test/java/br/com/systemsgs/aws_s3_spring_boot/AwsS3SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs.aws_s3_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
