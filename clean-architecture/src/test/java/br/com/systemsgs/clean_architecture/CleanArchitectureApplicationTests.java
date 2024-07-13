9a9bdb6f83e13c53999763f162e1986f7b50fb98
GuilhermeJWT <guiromanno@gmail.com>
2024-07-13 19:26:51 -0300

    ADD: Projeto - Clean Architecture

---
+++ clean-architecture/src/test/java/br/com/systemsgs/clean_architecture/CleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs.clean_architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
