b108463cb8a2641011e50fcf756cecf206b70bbe
GuilhermeJWT <guiromanno@gmail.com>
2024-09-21 22:08:32 -0300

    ADD: Projeto Strategy

---
+++ dp-strategy-springboot/src/main/java/br/com/systemsgs/dp_strategy_springboot/DpStrategySpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs.dp_strategy_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpStrategySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpStrategySpringbootApplication.class, args);
+	}
+
+}
---
+++ dp-strategy-springboot/src/test/java/br/com/systemsgs/dp_strategy_springboot/DpStrategySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs.dp_strategy_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpStrategySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
